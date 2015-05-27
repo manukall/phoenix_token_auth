e0ffc230457c5fa3286eab227ffce4b80d7e8c02
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-27 20:34:55 +0200

    Removed warning about unused vars

--- test/support/mailing_helper.exs
+++ test/support/mailing_helper.exs
@@ -2,10 +2,10 @@ defmodule PhoenixTokenAuth.TestMailing do
 	@behaviour PhoenixTokenAuth.MailingBehaviour
 
 	def welcome_subject(user), do: "Hello #{user.email}"
-	def welcome_body(user, token), do: token		
+	def welcome_body(_user, token), do: token		
 	def password_reset_subject(user), do: "Hello #{user.email}"
-	def password_reset_body(user, token), do: token		
+	def password_reset_body(_user, token), do: token		
 	def new_email_address_subject(user), do: "Hello #{user.email}"
-	def new_email_address_body(user, token), do: token		
+	def new_email_address_body(_user, token), do: token		
 	
 end
