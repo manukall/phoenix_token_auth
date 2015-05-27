4bb27d7aa6cd551a2d98e5b7acffd09e840de601
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-27 20:50:44 +0200

    implement the correct test settings

--- test/support/mailing_helper.exs
+++ test/support/mailing_helper.exs
@@ -2,10 +2,10 @@ defmodule PhoenixTokenAuth.TestMailing do
 	@behaviour PhoenixTokenAuth.MailingBehaviour
 
 	def welcome_subject(user), do: "Hello #{user.email}"
-	def welcome_body(_user, token), do: token		
+	def welcome_body(_user, _token), do: "the_emails_body" 		
 	def password_reset_subject(user), do: "Hello #{user.email}"
-	def password_reset_body(_user, token), do: token		
-	def new_email_address_subject(user), do: "Hello #{user.email}"
-	def new_email_address_body(_user, token), do: token		
+	def password_reset_body(_user, _token), do: "the_emails_body" 		
+	def new_email_address_subject(_user), do: "Please confirm your email address"
+	def new_email_address_body(_user, token), do: token
 	
 end
