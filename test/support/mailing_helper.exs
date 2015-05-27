5a33ef7b0ea498608d4d0c400f876c941e75cbcb
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-27 20:32:55 +0200

    implementation of the mailing behaviour for tests

---
+++ test/support/mailing_helper.exs
@@ -0,0 +1,11 @@
+defmodule PhoenixTokenAuth.TestMailing do
+	@behaviour PhoenixTokenAuth.MailingBehaviour
+
+	def welcome_subject(user), do: "Hello #{user.email}"
+	def welcome_body(user, token), do: token		
+	def password_reset_subject(user), do: "Hello #{user.email}"
+	def password_reset_body(user, token), do: token		
+	def new_email_address_subject(user), do: "Hello #{user.email}"
+	def new_email_address_body(user, token), do: token		
+	
+end
