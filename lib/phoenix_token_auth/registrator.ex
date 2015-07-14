76fecc112ba4fad9deb9568712eb7bf616ae7502
Alexandre Pereira <pereira.alex@gmail.com>
2015-07-14 18:20:11 +0100

    reworked registering function and created changeset_helper
    add username to User model in tests

--- lib/phoenix_token_auth/registrator.ex
+++ lib/phoenix_token_auth/registrator.ex
@@ -7,23 +7,26 @@ defmodule PhoenixTokenAuth.Registrator do
   Returns a changeset setting email and hashed_password on a new user.
   Validates that email and password are present and that email is unique.
   """
-  def changeset(params) do
+  def changeset(params = %{"username" => username}) when username != "" and username != nil do
+    Changeset.cast(struct(UserHelper.model), params, ~w(username))
+    |> Changeset.validate_change(:username, &Util.presence_validator/2)
+    |> Changeset.validate_unique(:username, on: Util.repo)
+    |> Changeset.put_change(:hashed_confirmation_token, nil)
+    |> Changeset.put_change(:confirmed_at, Ecto.DateTime.utc)
+    |> changeset_helper
+  end
 
-    if params["username"] do
-      Changeset.cast(struct(UserHelper.model), params, ~w(username))
-      |> Changeset.validate_change(:username, &Util.presence_validator/2)
-      |> Changeset.validate_unique(:username, on: Util.repo)
-      |> UserHelper.validator
-      |> set_hashed_password
-      |> Changeset.put_change(:hashed_confirmation_token, nil)
-      |> Changeset.put_change(:confirmed_at, Ecto.DateTime.utc)
-    else
-      Changeset.cast(struct(UserHelper.model), params, ~w(email))
-      |> Changeset.validate_change(:email, &Util.presence_validator/2)
-      |> Changeset.validate_unique(:email, on: Util.repo)
-      |> UserHelper.validator
-      |> set_hashed_password
-    end
+  def changeset(params) do
+    Changeset.cast(struct(UserHelper.model), params, ~w(email))
+    |> Changeset.validate_change(:email, &Util.presence_validator/2)
+    |> Changeset.validate_unique(:email, on: Util.repo)
+    |> changeset_helper
+  end
+
+  def changeset_helper(changeset) do
+    changeset
+    |> UserHelper.validator
+    |> set_hashed_password
   end
 
   def set_hashed_password(changeset = %{errors: [_]}), do: changeset
