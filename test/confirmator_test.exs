ccb23e4713fad3423b21ccebc33b816a4bb874c3
schainker <david@schainker>
2015-07-21 10:00:18 +0800

    pass dummy user to confirmation_needed_changset

--- test/confirmator_test.exs
+++ test/confirmator_test.exs
@@ -6,10 +6,10 @@ defmodule ConfirmatorTest do
 
 
   test "confirmation_needed_changeset adds the hashed token" do
-    {token, changeset} = %Ecto.Changeset{}
+    {token, user} = Forge.user()
       |> Ecto.Changeset.cast(:empty, [], [])
       |> Confirmator.confirmation_needed_changeset()
-    hashed_confirmation_token = Ecto.Changeset.get_change(changeset, :hashed_confirmation_token)
+    hashed_confirmation_token = Ecto.Changeset.get_change(user, :hashed_confirmation_token)
 
     assert crypto_provider.checkpw(token, hashed_confirmation_token)
   end
