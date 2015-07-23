aac197ecb13ca1ac7dfb7115d83d7215f82d39aa
schainker <david@schainker>
2015-07-23 16:01:14 +0800

    remove useless config params

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :my_app, Repo,
-  pool: Ecto.Adapters.SQL.Sandbox
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
