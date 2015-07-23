e8c257de942fa8ed48158708608898c3cdf61214
Manuel Kallenbach <mk@manukall.de>
2015-07-23 19:42:55 +0200

    version 0.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixTokenAuth.Mixfile do
 
   def project do
     [app: :phoenix_token_auth,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.0",
      package: package,
      description: description,
